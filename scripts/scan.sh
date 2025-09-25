35514de6002745ab08c04fb7fc5e0a3f74023dfa
Khaz713 <konrad713@gmail.com>
2025-09-25 14:53:20 +0200

    N: Revert M
    This reverts commit c494243883499ae1a67f37393d430884c57e170f.

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,10 +1,10 @@
 printf "\n====== SCANNING FOR CREDIT CARD NUMBERS ======\n"
-grep -rEn --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git}
+grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git}
 echo "========= CREDIT CARD SCAN COMPLETE =========="
 printf "\n==== SCANNING FOR SOCIAL SECURITY NUMBERS ====\n"
-grep -rEn --color=always '\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git}
+grep -rE --color=always '\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git}
 echo "======= SOCIAL SECURITY SCAN COMPLET"
 printf "\n========= SCANNING FOR PHONE NUMBERS =========\n"
-grep -rEn --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git}
-grep -rEn --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git}
+grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git}
+grep -rE --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git}
 echo "========= PHONE NUMBER SCAN COMPLETE ========="
